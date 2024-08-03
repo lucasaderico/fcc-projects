841c1ec70ed8f83bdb8aaae6ed86a0f83d061a98
Lucas Aderico <lucaoliveira57@gmail.com>
2024-08-03 20:09:55 -0300

    Add calorie-counter project

---
+++ javascript-algorithms-and-data-structures/calorie-counter/script.js
@@ -0,0 +1,118 @@
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`,
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input
+      type="number"
+      min="0"
+      id="${entryDropdown.value}-${entryNumber}-calories"
+      placeholder="Calories"
+    />`;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+  return calories;
+}
+
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+
+  const breakfastNumberInput = document.querySelectorAll(
+    "#breakfast input[type=number]",
+  );
+  const lunchNumberInput = document.querySelectorAll(
+    "#lunch input[type=number]",
+  );
+  const dinnerNumberInput = document.querySelectorAll(
+    "#dinner input[type=number]",
+  );
+  const snacksNumberInput = document.querySelectorAll(
+    "#snacks input[type=number]",
+  );
+  const exerciseNumberInput = document.querySelectorAll(
+    "#exercise input[type=number]",
+  );
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInput);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInput);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInput);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInput);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInput);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+  if (isError) {
+    return;
+  }
+
+  const consumedCalories =
+    breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+  output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calories ${surplusOrDeficit}</span>
+    <hr />
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+  `;
+
+  output.classList.remove("hide");
+}
+
+function clearForm() {
+  const inputContainers = Array.from(
+    document.querySelectorAll(".input-container"),
+  );
+
+  for (const container of inputContainers) {
+    container.innerHTML = "";
+  }
+
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add("hide");
+}
+
+addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
