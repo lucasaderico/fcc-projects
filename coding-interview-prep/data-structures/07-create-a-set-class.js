04a9c895bc256bb66a435e83265cb9a37c4f62e5
Lucas Aderico <lucaoliveira57@gmail.com>
2025-06-21 22:59:13 -0300

    Add 'create-a-set-class' challenge

---
+++ coding-interview-prep/data-structures/07-create-a-set-class.js
@@ -0,0 +1,43 @@
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-set-class
+
+class Set {
+  constructor() {
+    // Dictionary will hold the items of our set
+    this.dictionary = {};
+    this.length = 0;
+  }
+
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+
+  // This method will return all the values in the set
+  values() {
+    return Object.values(this.dictionary);
+  }
+
+  add(element) {
+    if (this.has(element)) {
+      return false;
+    }
+
+    this.dictionary[element] = element;
+    this.length++;
+    return true;
+  }
+
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+
+  size() {
+    return this.length;
+  }
+}
